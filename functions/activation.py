Fixed issue with errors not correctly backpropagating.

functions/activation.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def sigmoid(x,  derivative=False):
     if derivative:
-        return np.exp(-x) * np.square(sigmoid(x))
+        return sigmoid(x) * (1 - sigmoid(x))
     return 1 / (1 + np.exp(-x))
 
 

functions/loss.py
@@ -3,15 +3,18 @@ import numpy as np
 
 def mean_squared_error(y_predicted, y_true, differentiate):
     if differentiate:
-            return 2 * (np.square(np.subtract(y_true, y_predicted)))
-    return np.square(np.subtract(y_true, y_predicted)) / 2
+            return 2 * (np.subtract(y_predicted, y_true)) / y_true.size
+    return np.square(np.subtract(y_predicted, y_true)) / y_true.size
 
 
 def binary_cross_entropy(y_predicted, y_true, differentiate):
     y_predicted = np.clip(y_predicted, 1e-12, 1 - 1e-12)
     if differentiate:
-        return -np.dot(np.transpose(y_true), 1 / y_predicted)\
-                      - np.dot(np.transpose(1 - y_true), 1 / (1 - y_predicted))
-    return -np.dot(np.transpose(y_true), np.log(y_predicted))\
-           - np.dot(np.transpose(1 - y_true), np.log(1 - y_predicted))
+        return -y_true / y_predicted - (1 - y_true) / (1 - y_predicted)
+    return -y_true * np.log(y_predicted) - (1 - y_true) * np.log(1 - y_predicted)
+    #if differentiate:
+    #    return -np.dot(np.transpose(y_true), 1 / y_predicted)\
+    #                  - np.dot(np.transpose(1 - y_true), 1 / (1 - y_predicted))
+    #return -np.dot(np.transpose(y_true), np.log(y_predicted))\
+    #       - np.dot(np.transpose(1 - y_true), np.log(1 - y_predicted))
 

layer.py
@@ -1,12 +1,8 @@
 import numpy as np
-from multiprocessing import Pool
 import functions.activation
 import functions.pooling
 import functions.loss
 import abc
-
-USE_MULTITHREADING = True
-NUM_THREADS = 8
 
 
 class Layer(abc.ABC):
@@ -34,11 +30,8 @@ class Layer(abc.ABC):
         self.next = []
 
     def vectorize(self):
-        weights = np.empty(0)
-        gradients = np.empty(0)
-        if not isinstance(self, Input) and not isinstance(self, Pooling):
-            weights = self.weights.flatten()
-            gradients = self.gradient.flatten()
+        weights = self.weights.flatten() if self.weights is not None else np.zeros(0)
+        gradients = self.gradient.flatten() if self.gradient is not None else np.zeros(0)
         for layer in self.next:
             if layer is None:
                 continue
@@ -48,31 +41,22 @@ class Layer(abc.ABC):
         return weights, gradients
 
     def update_weights(self, weights, update_next):
-        if isinstance(self, Input):
-            for layer in self.next:
-                weights = layer.update_weights(weights, True)
-        else:
+        if self.weights is not None:
             length = len(self)
             self.weights = weights[:length].reshape(self.weights.shape)
-            if update_next:
-                for layer in self.next:
-                    if layer is not None:
-                        weights = layer.update_weights(weights[length:], True)
-                return weights[length:]
-
-    def _calculate_product_sums(self, xi):
-        x, i = xi
-        self.product_sum[i] = self.bias
-        for j in range(self.input_length):
-            self.product_sum[i] += np.reshape(x, self.input_length)[j] * self.weight_value(j, i)
+            weights = weights[length:]
+        if update_next:
+            for layer in self.next:
+                if layer is not None:
+                    weights = layer.update_weights(weights, True)
+            return weights
 
     def update_product_sums(self, x):
         self.product_sum = np.empty(self.output_length)
-        if USE_MULTITHREADING:
-            Pool(NUM_THREADS).map(self._calculate_product_sums, [(x, i) for i in range(self.output_length)])
-        else:
-            for i in range(self.output_length):
-                self._calculate_product_sums(x, i)
+        for i in range(self.output_length):
+            self.product_sum[i] = 0
+            for j in range(self.input_length):
+                self.product_sum[i] += np.reshape(x, self.input_length)[j] * self.weight_value(j, i)
 
     def forward(self, x):
         self.outputs = np.zeros(self.output_length)
@@ -87,48 +71,26 @@ class Layer(abc.ABC):
             if layer is not None:
                 layer.forward(x)
 
-    def print_next(self):
-        if isinstance(self, Input):
-            print("%0.3f ---> %0.3f\t(Input)" % (self.inputs[1], self.outputs[1]))
-        else:
-            print(type(self))
-            print("%0.3f ---> %0.3f\t(%0.3f)" % (self.inputs[1], self.outputs[1], self.product_sum[1]))
-        for layer in self.next:
-            if layer is not None:
-                layer.print_next()
-
     def update(self, error=None):
-        self.update_error(error)
-        self.update_gradient()
+        if self.weights is not None:
+            self.update_error(error)
+            self.update_gradient()
         if self.last is not None:
             for layer in self.last:
-                if type(layer) != Input:
-                    layer.update()
-
-    def _calculate_error(self, i, j):
-        error_sum = 0
-        for layer in self.next:
-            error_sum += np.sum([layer.weight_value(i, j) * layer.error_value(j)
-                                 for j in range(layer.output_length)])
-
-    def _calculate_error(self, i):
-        error_sum = 0
-        for layer in self.next:
-            error_sum += np.sum([layer.weight_value(i, j) * layer.error_value(j)
-                                 for j in range(layer.output_length)])
-        self.error[i] = self.activation(self.product_sum[i], True) * error_sum
+                layer.update()
 
     def update_error(self, error):
         if error is None:
             self.error = np.zeros(self.output_length)
-            if USE_MULTITHREADING:
-                error_pool = Pool(NUM_THREADS)
-                error_pool.map(self._calculate_error, [i for i in range(self.output_length)])
-            else:
-                for i in range(self.output_length):
-                    self._calculate_error(i)
+            for i in range(self.output_length):
+                error_sum = 0
+                for layer in self.next:
+                    error_sum += np.sum([layer.weight_value(i, j) * layer.error_value(j)
+                                         for j in range(layer.output_length)])
+                self.error[i] = self.activation(self.product_sum[i], True) * error_sum
         else:
-            self.error = error
+            self.error = [error[i] * self.activation(self.product_sum[i], True)
+                          for i in range(self.output_length)]
 
     def update_gradient(self):
         self.gradient = np.empty(self.weights.shape if self.gradient_shape is None else self.gradient_shape)
@@ -176,7 +138,7 @@ class Layer(abc.ABC):
         return config
 
     def __len__(self):
-        return self.input_length * self.output_length
+        return self.weights.size
 
     def __str__(self):
         return "%s(%s)" % (self.__class__.__name__, self.name)
@@ -199,9 +161,6 @@ class Input(Layer):
         self.inputs = x
         self.compute_next(x)
 
-    def gradient_value(self, i, j, update_value=None):
-        pass
-
 
 class Intersection(Layer):
 
@@ -221,7 +180,7 @@ class Intersection(Layer):
 
 class Dense(Layer):
 
-    def __init__(self, output_shape, activation=functions.activation.sigmoid, bias=0):
+    def __init__(self, output_shape, activation=functions.activation.relu, bias=0):
         super(Dense, self).__init__(output_shape, activation, bias)
         self.input_length = None
 
@@ -238,7 +197,7 @@ class Dense(Layer):
 class Filter(Layer):
 
     def __init__(self, inputs_shape, filters, size, stride, padding,
-                 activation=functions.activation.sigmoid):
+                 activation=functions.activation.relu):
         self.output_width = (inputs_shape[0] - size + 2 * padding) // stride + 1
         self.output_height = (inputs_shape[1] - size + 2 * padding) // stride + 1
         self.output_depth = inputs_shape[2]

nn.py
@@ -36,12 +36,13 @@ class NeuralNetwork:
         y_predicted = self.predict(x)
         self.output_layer.update(self.loss(y_predicted, y_true, False)
                                  * self.output_layer.activation(y_predicted, True))
-        return np.mean(self.loss(y_predicted, y_true, False))
-    
+        return self.loss(y_predicted, y_true, False)
 
     def save_weights(self, path):
         weights, _ = self.vectorize()
         with open(path, 'w') as file:
+            np.zeros(3, dtype=np.int8)
+            {}.keys()
             json.dump(weights.tolist(), file, indent=4)
             file.close()
 
@@ -50,3 +51,6 @@ class NeuralNetwork:
             weights = json.load(file)
             file.close()
         self.update_weights(weights)
+
+    def reset(self):
+        self.input_layer.reset()

optimizer.py
@@ -20,6 +20,7 @@ class Optimizer(abc.ABC):
                     progress.update(1)
                 progress.close()
                 self.optimization_end()
+        neural_network.reset()
 
     @abc.abstractmethod
     def optimization_iteration(self, neural_network, x_batch, y_batch, iteration):
@@ -40,9 +41,8 @@ class SGD(Optimizer):
     def optimization_iteration(self, neural_network, x_batch, y_batch, iteration):
         output_loss = neural_network.calculate_gradient(x_batch, y_batch)
         weights, gradient = neural_network.vectorize()
-        weights -= self.learning_rate * gradient
-        neural_network.update_weights(weights)
-        return {'loss': output_loss}
+        neural_network.update_weights(weights - self.learning_rate * gradient)
+        return {'loss': np.sum(output_loss)}
 
 
 class Adam(Optimizer):
@@ -67,7 +67,7 @@ class Adam(Optimizer):
         self.past_m = m
         self.past_v = v
         neural_network.update_weights(weights)
-        return {'loss': output_loss}
+        return {'loss': np.sum(output_loss)}
 
     def optimization_end(self):
         self.past_m = 0

testing.py
@@ -1,5 +1,4 @@
 from nn import NeuralNetwork
-import numpy as np
 import functions.activation
 import functions.pooling
 import functions.loss
@@ -11,35 +10,17 @@ import layer
 def mnist_test(epochs):
     x_train, y_train = datasets.load_mnist()
     inputs = layer.Input((28, 28, 1))
-    filter_one = layer.Filter((28, 28, 1), 4, 3, 1, 1)(inputs)
-    c_dense = layer.Dense(64)(filter_one)
-    outputs = layer.Dense(10, activation=functions.activation.sigmoid)(c_dense)
+    filter_one = layer.Filter((28, 28, 1), 16, 3, 1, 1)(inputs)
+    intermediate_layer = layer.Dense(16, activation=functions.activation.sigmoid)(filter_one)
+    outputs = layer.Dense(10, activation=functions.activation.sigmoid)(intermediate_layer)
     model = NeuralNetwork(inputs, outputs, optimizer.SGD(), functions.loss.binary_cross_entropy)
-    model.fit(x_train, y_train, epochs)
-    model.save_weights("mnist weights.json")
-
-
-def square_test(epochs, training_length):
-    inputs = layer.Input((28, 28, 1))
-    filter_one = layer.Filter((28, 28, 1), 2, 3, 1, 1)(inputs)
-    c_dense = layer.Dense(4)(filter_one)
-    outputs = layer.Dense(1, activation=functions.activation.sigmoid)(c_dense)
-    model = NeuralNetwork(inputs, outputs, optimizer.SGD(), functions.loss.mean_squared_error)
-    x_train = []
-    y_train = []
-    for i in range(training_length):
-        x = np.sort(np.random.uniform(size=784))
-        total = 0
-        for j in range(x.size):
-            total += 1 - x[j]
-        y = total / x.size
-        x_train.append(x)
-        y_train.append([y])
     print(model.predict(x_train[0]))
+    model.save_weights("mnist_initial_weights.json")
     model.fit(x_train, y_train, epochs)
-    model.save_weights("weight_output.json")
+    model.save_weights("mnist_final_weights.json")
+    print(y_train[0])
+    print(model.predict(x_train[0]))
 
 
 if __name__ == '__main__':
-    #mnist_test(1)
-    square_test(5, 250)
+    mnist_test(1)
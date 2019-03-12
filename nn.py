4e50d01835faabcbd7ce8de19acd2e2819cb099b
David <DavidO256@users.noreply.github.com>
2019-03-12 09:57:23 -0400

    Added basic serialization support.

--- nn.py
+++ nn.py
@@ -2,6 +2,18 @@ import numpy as np
 import json
 
 
+def initialize_names(counts, layer):
+    if layer.name is None:
+        if type(layer) not in counts:
+            counts[type(layer)] = 0
+        layer.name = "%s_%s" % (layer.__class__.__name__.lower(), counts[type(layer)])
+        counts[type(layer)] += 1
+    if layer.next is not None:
+        for n in layer.next:
+            if n is not None:
+                initialize_names(counts, n)
+
+
 class NeuralNetwork:
 
     def __init__(self, input_layer, output_layer, optimizer, loss, metrics=None):
@@ -14,6 +26,7 @@ class NeuralNetwork:
         self.input_layer.last = None
         self.output_layer.next = [None]
         self.output_layer.initialize()
+        initialize_names({}, self.input_layer)
 
     def vectorize(self):
         return self.input_layer.vectorize()
@@ -21,36 +34,49 @@ class NeuralNetwork:
     def update_weights(self, weights):
         self.input_layer.update_weights(weights, True)
 
-    def fit(self, x, y, epochs):
-        self.optimizer.optimize(self, epochs, x, y)
+    def fit(self, x, y, epochs, batch_size=16):
+        self.optimizer.optimize(self, epochs, batch_size, x, y)
 
     def predict(self, x):
         self.input_layer.forward(x)
         return self.output_layer.outputs
 
-    def calculate_gradient(self, x, y_true):
+    def calculate_gradient(self, x, y_true, pool=None):
         if not isinstance(x, np.ndarray):
             return self.calculate_gradient(np.asarray(x), y_true)
         if not isinstance(y_true, np.ndarray):
             return self.calculate_gradient(x, np.asarray(y_true))
         y_predicted = self.predict(x)
         self.output_layer.update(self.loss(y_predicted, y_true, False)
-                                 * self.output_layer.activation(y_predicted, True))
+                                 * self.output_layer.activation(y_predicted, True), pool)
         return self.loss(y_predicted, y_true, False)
 
     def save_weights(self, path):
         weights, _ = self.vectorize()
         with open(path, 'w') as file:
-            np.zeros(3, dtype=np.int8)
-            {}.keys()
             json.dump(weights.tolist(), file, indent=4)
+            file.close()
+
+    def save_model(self, path):
+        configuration = self.serialize()
+        with open(path, 'w') as file:
+            json.dump(configuration, file, indent=4)
             file.close()
 
     def load_weights(self, path):
         with open(path) as file:
             weights = json.load(file)
             file.close()
-        self.update_weights(weights)
+        self.update_weights(np.asarray(weights))
+
+    def serialize(self):
+        return {
+            'loss': self.loss.__name__,
+            'input': self.input_layer.name,
+            'output': self.output_layer.name,
+            'optimizer': self.optimizer.serialize(),
+            'layers': self.input_layer.serialize(None)
+        }
 
     def reset(self):
         self.input_layer.reset()

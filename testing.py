04a130e646b1e54612efbf150c15762a7f19c866
David <DavidO256@users.noreply.github.com>
2019-03-12 09:58:12 -0400

    Updated testing.py

--- testing.py
+++ testing.py
@@ -8,19 +8,17 @@ import layer
 
 
 def mnist_test(epochs):
-    x_train, y_train = datasets.load_mnist()
+    x_train, y_train = datasets.load_mnist(1)
     inputs = layer.Input((28, 28, 1))
-    filter_one = layer.Filter((28, 28, 1), 16, 3, 1, 1)(inputs)
-    intermediate_layer = layer.Dense(16, activation=functions.activation.sigmoid)(filter_one)
-    outputs = layer.Dense(10, activation=functions.activation.sigmoid)(intermediate_layer)
-    model = NeuralNetwork(inputs, outputs, optimizer.SGD(), functions.loss.binary_cross_entropy)
-    print(model.predict(x_train[0]))
+    filter_layer = layer.Filter((28, 28, 1), 16, 3, 1, 1)(inputs)
+    outputs = layer.Dense(10, activation=functions.activation.softmax)(filter_layer)
+    model = NeuralNetwork(inputs, outputs, optimizer.Adam(),
+                          functions.loss.binary_cross_entropy)
     model.save_weights("mnist_initial_weights.json")
+    model.save_model("mnist_model.json")
     model.fit(x_train, y_train, epochs)
     model.save_weights("mnist_final_weights.json")
-    print(y_train[0])
-    print(model.predict(x_train[0]))
 
 
 if __name__ == '__main__':
-    mnist_test(1)
+    mnist_test(5)

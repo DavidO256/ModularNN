69b23310c7702fcbdb2ecf7826035ac4b195e615
David <DavidO256@users.noreply.github.com>
2019-03-12 09:47:42 -0400

    Added softmax activation function.

--- functions/activation.py
+++ functions/activation.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 
+def get_function(name):
+    return {'sigmoid': sigmoid, 'relu': relu,
+            'softmax': softmax}[name]
+
+
 def sigmoid(x,  derivative=False):
     if derivative:
         return sigmoid(x) * (1 - sigmoid(x))
@@ -12,3 +17,8 @@ def relu(x, derivative=False):
         return (x > 0) * 1.0
     return x * (x > 0)
 
+
+def softmax(x, derivative=True):
+    assert isinstance(x, np.ndarray)
+    exp = np.exp(x - np.max(x))
+    return exp / np.sum(exp)

Added multithreading to error and gradient calculation. Fixed filter output shape. Output activations now calculate as a whole. Added layer serialization. Various bug fixes.

layer.py
@@ -2,6 +2,7 @@ import numpy as np
 import functions.activation
 import functions.pooling
 import functions.loss
+import itertools
 import abc
 
 
@@ -51,19 +52,19 @@ class Layer(abc.ABC):
                     weights = layer.update_weights(weights, True)
             return weights
 
-    def update_product_sums(self, x):
+    def update_product_sums(self, x, pool=None):
         self.product_sum = np.empty(self.output_length)
+        inputs = np.reshape(x, self.input_length)
         for i in range(self.output_length):
             self.product_sum[i] = 0
             for j in range(self.input_length):
-                self.product_sum[i] += np.reshape(x, self.input_length)[j] * self.weight_value(j, i)
+                self.product_sum[i] += inputs[j] * self.weight_value(j, i)
 
     def forward(self, x):
         self.outputs = np.zeros(self.output_length)
         self.update_product_sums(x)
-        self.inputs = x
-        self.outputs = np.asarray([self.activation(self.product_sum.flatten()[i])
-                                   for i in range(self.output_length)])
+        self.inputs = np.reshape(x, self.inputs_shape)
+        self.outputs = self.activation(self.product_sum.flatten())
         self.compute_next(self.outputs)
 
     def compute_next(self, x):
@@ -71,32 +72,50 @@ class Layer(abc.ABC):
             if layer is not None:
                 layer.forward(x)
 
-    def update(self, error=None):
+    def update(self, error=None, pool=None):
         if self.weights is not None:
-            self.update_error(error)
-            self.update_gradient()
+            self.update_error(error, pool)
+            self.update_gradient(None)
         if self.last is not None:
             for layer in self.last:
                 layer.update()
 
-    def update_error(self, error):
+    def _calculate_error(self, index, g_prime):
+        error_sum = 0
+        for layer in self.next:
+            error_sum += np.sum([layer.weight_value(index, j) * layer.error_value(j)
+                                 for j in range(layer.output_length)])
+        self.error[index] = g_prime[index] * error_sum
+
+    def update_error(self, error, pool=None):
+        output_activation = self.activation(self.product_sum, True)
         if error is None:
             self.error = np.zeros(self.output_length)
-            for i in range(self.output_length):
-                error_sum = 0
-                for layer in self.next:
-                    error_sum += np.sum([layer.weight_value(i, j) * layer.error_value(j)
-                                         for j in range(layer.output_length)])
-                self.error[i] = self.activation(self.product_sum[i], True) * error_sum
+            if pool is None:
+                for i in range(self.output_length):
+                    self._calculate_error(i, output_activation)
+            else:
+                pool.starmap(self._calculate_error, zip(range(self.output_length), itertools.repeat(output_activation)))
         else:
-            self.error = [error[i] * self.activation(self.product_sum[i], True)
+            self.error = [error[i] * output_activation[i]
                           for i in range(self.output_length)]
 
-    def update_gradient(self):
+    def _calculate_gradient(self, index):
+        i, j = index
+        self.gradient_value(i, j, update_value=self.inputs[i] * self.error[j])
+
+    def update_gradient(self, pool=None):
         self.gradient = np.empty(self.weights.shape if self.gradient_shape is None else self.gradient_shape)
-        for i in range(self.input_length):
-            for j in range(self.output_length):
-                self.gradient_value(i, j, update_value=self.inputs[i] * self.error[j])
+        if pool is None:
+            for i in range(self.input_length):
+                for j in range(self.output_length):
+                    self._calculate_gradient((i, j))
+        else:
+            indices = []
+            for i in range(self.input_length):
+                for j in range(self.output_length):
+                    indices.append((i, j))
+            pool.map(self._calculate_gradient, indices)
 
     def initialize(self):
         if self.last is not None:
@@ -114,7 +133,8 @@ class Layer(abc.ABC):
         self.gradient = None
         if self.next is not None:
             for layer in self.next:
-                layer.reset()
+                if layer is not None:
+                    layer.reset()
 
     def error_value(self, index):
         return self.error[index]
@@ -126,16 +146,32 @@ class Layer(abc.ABC):
             self.last.append(layer)
         return self
 
-    def serialize(self):
-        config = {'activation': self.activation,
-                  'output_length': self.output_length,
-                  'next': None,
-                  'last': None}
+    def serialize(self, config):
+        if config is None:
+            return self.serialize({})
+        if self.name not in config:
+            layer_config = {
+                'bias': self.bias,
+                'name': self.name,
+                'type': type(self).__name__,
+                'activation': self.activation.__name__ if self.activation is not None else None,
+                'inputs_shape': self.inputs_shape,
+                'outputs_shape': self.outputs_shape,
+                'next': [layer.name if layer is not None else None
+                         for layer in self.next] if self.next is not None else None,
+                'last': [layer.name if layer is not None else None
+                         for layer in self.last] if self.last is not None else None
+            }
+            self.serialize_custom(layer_config)
+            config[self.name] = layer_config
         if self.next is not None:
-            config['next'] = [layer.name for layer in self.next]
-        if self.last is not None:
-            config['last'] = [layer.name for layer in self.last]
+            for layer in self.next:
+                if layer is not None:
+                    layer.serialize(config)
         return config
+
+    def serialize_custom(self, layer_config):
+        pass
 
     def __len__(self):
         return self.weights.size
@@ -190,6 +226,10 @@ class Dense(Layer):
         else:
             self.gradient[i][j] = update_value
 
+    def initialize(self):
+        super(Dense, self).initialize()
+        self.inputs_shape = (int(self.input_length),)
+
     def weight_value(self, input_index, output_index):
         return self.weights[input_index % self.input_length][output_index % self.output_length]
 
@@ -201,7 +241,7 @@ class Filter(Layer):
         self.output_width = (inputs_shape[0] - size + 2 * padding) // stride + 1
         self.output_height = (inputs_shape[1] - size + 2 * padding) // stride + 1
         self.output_depth = inputs_shape[2]
-        super(Filter, self).__init__((filters, self.output_width, self.output_height),
+        super(Filter, self).__init__((self.output_width, self.output_height, filters),
                                      activation)
         self.inputs_shape = inputs_shape
         self.padding = padding
@@ -209,7 +249,7 @@ class Filter(Layer):
         self.stride = stride
         self.size = size
 
-    def update_gradient(self):
+    def update_gradient(self, pool=None):
         self.gradient = np.zeros(self.weights.shape)
         inputs = np.pad(self.inputs.reshape(self.inputs_shape), self.padding, 'constant', constant_values=0)
         for f in range(self.filters):
@@ -222,28 +262,26 @@ class Filter(Layer):
                             for n in range(self.size):
                                 self.gradient[f][m][n][k] += inputs[i_x + m - self.size // 2][j_y + n - self.size // 2][k] * self.error[f][i][j][k]
 
-    def update_error(self, error):
+    def update_error(self, error, pool=None):
         if error is None:
-            def convert(a, b, c, d): return self.output_depth\
-                                            * (self.output_height * (self.output_width * a + b) + c) + d
             self.error = np.zeros(shape=(self.filters, self.output_width, self.output_height, self.output_depth))
 
+            output_activation = self.activation(self.product_sum, True).flatten()
             for f in range(self.filters):
                 for i in range(self.output_width):
                     for j in range(self.output_height):
                         for k in range(self.output_depth):
                             error_sum = 0
                             for layer in self.next:
-                                error_sum += np.sum([layer.weight_value(convert(f, i, j, k), o)
-                                                     * layer.error_value(o)
-                                                     for o in range(layer.output_length)])
-                            self.error[f][i][j][k] = error_sum * self.activation(self.product_sum[convert(f, i, j, k)
-                                                                                 % self.product_sum.size], True)
+                                error_sum += np.sum([layer.weight_value(self.convert_index(f, i, j, k), o)
+                                                     * layer.error_value(o) for o in range(layer.output_length)])
+                            self.error[f][i][j][k] = output_activation[self.convert_index(f, i, j, k)
+                                                                       % self.product_sum.size] * error_sum
         else:
             self.error = np.reshape(error, self.outputs_shape)
 
-    def update_product_sums(self, x):
-        x = np.pad(x.reshape(self.inputs_shape), self.padding, 'constant', constant_values=0)
+    def update_product_sums(self, x, pool=None):
+        x = np.pad(x.reshape(self.inputs_shape), ((self.padding,), (self.padding,), (0,)), 'constant', constant_values=0)
         result = np.zeros(shape=(self.filters, self.output_width, self.output_height, self.inputs_shape[2]))
         product_sum = np.zeros(shape=(self.filters, self.output_width, self.output_height))
         for f in range(self.filters):
@@ -254,17 +292,16 @@ class Filter(Layer):
                         j_y = self.stride * (j - 1) + self.size - 2 * self.padding
                         for m in range(self.size):
                             for n in range(self.size):
-                                result[f][i][j] += x[i_x + m - self.size // 2][j_y + n - self.size // 2][k] \
-                                                   * self.weights[f][m][n][k]
+                                result[f][i][j][k] += x[i_x + m - self.size // 2][j_y + n - self.size // 2][k] * \
+                                                      self.weights[f][m][n][k]
             product_sum[f] = np.sum(result[f], axis=2) + self.bias
-        self.product_sum = product_sum.flatten()
+        self.product_sum = product_sum.transpose()
 
     def forward(self, x):
         self.outputs = np.zeros(self.output_length)
         self.update_product_sums(x)
-        self.inputs = x
-        self.outputs = np.asarray([self.activation(self.product_sum.flatten()[i])
-                                   for i in range(self.product_sum.size)])
+        self.inputs = np.reshape(x, self.inputs_shape)
+        self.outputs = np.asarray(self.activation(self.product_sum.flatten()))
         self.compute_next(self.outputs)
 
     def weight_value(self, i, j):
@@ -277,6 +314,15 @@ class Filter(Layer):
     def initialize(self):
         super(Filter, self).initialize()
         self.weights = np.random.uniform(size=(self.filters, self.size, self.size, self.output_depth))
+
+    def serialize_custom(self, layer_config):
+        layer_config['padding'] = self.padding
+        layer_config['filters'] = self.filters
+        layer_config['stride'] = self.stride
+        layer_config['size'] = self.size
+
+    def convert_index(self, a, b, c, d):
+        return self.output_depth * (self.output_height * (self.output_width * a + b) + c) + d
 
     def __len__(self):
         return self.weights.size
@@ -296,7 +342,7 @@ class Pooling(Layer):
 
     def forward(self, x):
         self.outputs = np.zeros(self.outputs_shape)
-        self.indices = []
+        self.indices = {}
         x = np.reshape(x, self.inputs_shape)
         for i in range(self.outputs.shape[0]):
             for j in range(self.outputs.shape[1]):
@@ -305,12 +351,12 @@ class Pooling(Layer):
                                                          j * self.size:(j + 1) * self.size,
                                                          k])
                     self.outputs[i][j][k] = value
-                    self.indices.append((i * self.size + index[0],
-                                         j * self.size + index[1],
-                                         k))
+                    self.indices[self.convert_index(i, j, k)] = ((i * self.size + index[0],
+                                                                  j * self.size + index[1],
+                                                                  k))
         self.compute_next(self.outputs)
 
-    def update_error(self, error=None):
+    def update_error(self, error=None, pool=None):
         self.error = np.zeros(self.inputs_shape)
         for i, j, k, value in self.error:
             error_sum = 0
@@ -318,6 +364,7 @@ class Pooling(Layer):
                 error_sum += np.sum([layer.error_value(j)
                                      for j in range(layer.output_length)])
             self.error[i][j][k] = value * error_sum
+        self.error.flatten()
 
     def initialize(self):
         for layer in self.last:
@@ -328,3 +375,18 @@ class Pooling(Layer):
         if update_value is None:
             for layer in self.last:
                 layer.gradient_value(i, j, update_value)
+
+    def weight_value(self, input_index, output_index):
+        i, j, f = self.indices[output_index]
+        return self.last[0].weight_value(input_index, (i, j))
+
+    def error_value(self, index):
+        i, j, f = self.indices[index]
+        return self.last[0].error_value((f, i, j, 0))
+
+    def serialize_custom(self, layer_config):
+        layer_config['stride'] = self.stride
+        layer_config['size'] = self.size
+
+    def convert_index(self, i, j, k):
+        return self.outputs_shape[2] * (self.outputs_shape[1] * i + j) + k
cfd847d1ce95ed2561054375f930f5dc0dd45cb2
David <DavidO256@users.noreply.github.com>
2019-03-21 13:26:16 -0400

    Added support for running data in batches.

--- nn.py
+++ nn.py
@@ -16,15 +16,20 @@ def initialize_names(counts, layer):
 
 class NeuralNetwork:
 
-    def __init__(self, input_layer, output_layer, optimizer, loss, metrics=None):
+    def __init__(self, input_layer, output_layer, optimizer, loss, metrics=[]):
         self.input_layer = input_layer
         self.output_layer = output_layer
         self.optimizer = optimizer
         self.layers = []
         self.loss = loss
-        self.metrics = metrics
         self.input_layer.last = None
         self.output_layer.next = [None]
+        self.metrics = []
+        for m in metrics:
+            if m.__name__ == 'loss':
+                print("Skipping metric with reserved name \"loss\"")
+                continue
+            self.metrics.append(m)
         self.output_layer.initialize()
         initialize_names({}, self.input_layer)
 
@@ -34,21 +39,25 @@ class NeuralNetwork:
     def update_weights(self, weights):
         self.input_layer.update_weights(weights, True)
 
-    def fit(self, x, y, epochs, batch_size=16):
-        self.optimizer.optimize(self, epochs, batch_size, x, y)
+    def fit(self, x, y, epochs, batch_size=16, validation=None):
+        self.optimizer.optimize(self, x, y, epochs, batch_size, validation)
 
     def predict(self, x):
         self.input_layer.forward(x)
         return self.output_layer.outputs
 
-    def calculate_gradient(self, x, y_true, pool=None):
-        if not isinstance(x, np.ndarray):
-            return self.calculate_gradient(np.asarray(x), y_true)
-        if not isinstance(y_true, np.ndarray):
-            return self.calculate_gradient(x, np.asarray(y_true))
-        y_predicted = self.predict(x)
-        self.output_layer.update(self.loss(self.output_layer, y_true, False), pool)
-        return self.loss(self.output_layer, y_true, False)
+    def calculate_gradient(self, x_batch, y_batch, pool=None):
+        batch_derived_loss = []
+        batch_loss = []
+        for sample in range(len(x_batch)):
+            self.predict(x_batch[sample])
+            batch_derived_loss.append(self.loss(self.output_layer, y_batch[sample], True))
+            batch_loss.append(self.loss(self.output_layer, y_batch[sample], False))
+        self.output_layer.update(np.average(batch_derived_loss, axis=0), pool)
+        return np.average(batch_loss)
+
+    def calculate_metrics(self, y_predicted, y_true):
+        return {m.__name__: m(y_predicted, y_true) for m in self.metrics}
 
     def save_weights(self, path):
         weights, _ = self.vectorize()
